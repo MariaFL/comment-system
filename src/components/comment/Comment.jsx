8079e4fe3306b006bad2d501df98edbf24ed0c3e
Maria Federova <fedmaria2@gmail.com>
2023-02-20 05:44:13 +0300

    edited text, added handle

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -3,12 +3,14 @@ import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
-import CommentText from './CommentText';
+import Collapse from '@mui/material/Collapse';
+
+
 
 const Comment = (props) => {
     const {avatar, author, rating, text} = props.comment;
     return (
-        <ListItem alignItems="flex-start">
+        <ListItem alignItems="flex-start" divider>
             <ListItemAvatar>
                 <Avatar src={avatar} alt={author}>
                     {!avatar ? author[0] : null}
@@ -16,10 +18,23 @@ const Comment = (props) => {
             </ListItemAvatar>
             <ListItemText
                 primary={author}
-                secondary={rating < -10 ? "NOOO" : text}
+                secondary={<CommentBlock text={text} rating={rating}/>}
             />
         </ListItem>
     );
+}
+
+const CommentBlock = ({text, rating}) => {
+    const [openText, setOpenText] = React.useState(rating > -10);
+
+    const handleOpenChange = () => setOpenText(true);
+
+    return (
+        <>
+            <Collapse in={!openText}><span onClick={handleOpenChange}>Открыть комментарий</span></Collapse>
+            <Collapse in={openText}><p>{text}</p></Collapse>
+        </>
+    )
 }
 
 export default Comment;
--- src/components/comment/CommentText.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ListItemText from '@mui/material/ListItemText';
-
-const CommentText = (props) => {
-    return props.text
-}
-
-export default CommentText;
